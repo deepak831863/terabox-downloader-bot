5ca9cd72de37c1cf1b9c7c91bb3ce5c96b2fbdd7
Deepak Kushwaha <deepak8005238@gmail.com>
2025-10-02 10:20:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 29277030  # api id
+API_HASH = "07885b2e5a680e98c287f1ebf8ff2a0e"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "8403128671:AAEYqyMJbk0n78Ho2ZvyUVEKOMe4VKrajUw"  # bot token
 
 
 # REDIS
